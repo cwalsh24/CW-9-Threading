ae560f569ad190696a19e40c5fa60434374e785b
cwalsh24 <cwalsh24@ut.utm.edu>
2023-03-01 10:59:19 -0600

    Add project files.

---
+++ ThreadingPractice/FindPiThread.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadingPractice
+{
+    //this is the class that you will create to hold thread state.  It will also house your thread function
+    public class FindPiThread
+    {
+    }
+}
