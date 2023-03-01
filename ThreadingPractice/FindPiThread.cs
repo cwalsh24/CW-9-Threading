5e6f72a15d2b10e3b395f097935a6b2f5cd95269
cwalsh24 <cwalsh24@ut.utm.edu>
2023-03-01 11:07:41 -0600

    Started adding stuff

--- ThreadingPractice/FindPiThread.cs
+++ ThreadingPractice/FindPiThread.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,27 @@ namespace ThreadingPractice
     //this is the class that you will create to hold thread state.  It will also house your thread function
     public class FindPiThread
     {
+        int dartNum;
+        int dartHits;
+        Random randomNum;
+
+        FindPiThread(int number)
+        {
+            randomNum = new Random();
+            dartNum = number;
+            dartHits = 0; 
+        }
+
+        public int getHits()
+        {
+            return dartHits;
+        }
+
+        public void throwDarts()
+        {
+            throw new NotImplementedException();
+            //Inside this function, you should have a loop that randomly “throws” every dart it should throw.
+            //After determining the x and y coordinates, increment your counter of how many land inside!
+        }
     }
 }
